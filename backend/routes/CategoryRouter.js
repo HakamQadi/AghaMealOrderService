fcf16cda095052bcf6bf3fb75797c82be51dc837
hakamqadi <hakam.h.qadi@gmail.com>
2025-09-06 22:45:15 +0300

    backend: update category logic

--- backend/routes/CategoryRouter.js
+++ backend/routes/CategoryRouter.js
@@ -1,11 +1,19 @@
 import express from "express";
 const router = express.Router();
 import categoryController from "../controller/CategoryController.js";
+import upload from "../utils/Multer.js";
 
 router.route("/").get(categoryController.getAllCategories);
-router.route("/add").post(categoryController.addCategory);
+
+router
+  .route("/add")
+  .post(upload.single("image"), categoryController.addCategory);
+
 // TODO check if there is already a category in the new name before changing it
-router.route("/update/:id").patch(categoryController.updateCategory);
+router
+  .route("/update/:id")
+  .patch(upload.single("image"), categoryController.updateCategory);
+
 router.route("/delete/:id").delete(categoryController.deleteCategory);
 
 export default router;
